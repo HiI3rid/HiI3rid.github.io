fbbecfcb5da5f33bd5edbfaf58045038a415bce6
HiI3rid <veres.vereszoltan@gmail.com>
2024-09-04 23:50:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,4 @@ document.addEventListener('DOMContentLoaded', () => {
         const button = dropdown.querySelector('.dropbtn');
         const content = dropdown.querySelector('.dropdown-content');
 
-        button.addEventListener('click', () => {
-            const isActive = content.style.display === 'block';
-
-            dropdowns.forEach(d => {
-                const c = d.querySelector('.dropdown-content');
-                if (d !== dropdown) {
-                    c.style.display = 'none';
-                }
-            });
-
-            content.style.display = isActive ? 'none' : 'block';
-        });
-    });
-});
+        button
